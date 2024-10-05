d09833ae6d3fa7206fa40a51b03a043f2eabe70e
Asad Mirza <asadbmirza97@gmail.com>
2024-10-05 13:40:32 -0400

    first draft of backend

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,97 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const db = require("../db/queries");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUserByUsername(username);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserById(id); // Corrected to use id instead of username
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const getUserProfile = (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send("You are not authenticated");
+  }
+  if (!req.user) {
+    return res.status(500).send("User data not found");
+  }
+  res.send(`Welcome ${req.user.username}`);
+};
+
+const postLogin = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" }); // Unauthorized
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Login failed" });
+      }
+      return res.status(200).json({ message: "Login successful" }); // Successful login
+    });
+  })(req, res, next);
+};
+
+const postRegister = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await db.createUser(username, email, hashedPassword);
+  } catch (err) {
+    res.status(500).send("An error occurred while registering");
+  }
+};
+
+const postLogout = (req, res) => {
+  req.logout();
+  if (err) {
+    return res.status(500).json({ message: "Logout failed" });
+  }
+  return res.status(200).send("Logged out successfully");
+};
+
+const getDashboard = (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send("You are not authenticated");
+  }
+  res.send(`Welcome to your dashboard, ${req.user.username}`);
+};
+
+
+
+module.exports = {
+  getUserProfile,
+  postLogin,
+  postLogout,
+  postRegister,
+  getDashboard,
+};
