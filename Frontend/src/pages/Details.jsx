14da4830ba9dd8487b4093b093a3f7b0a39f61de
Asad Mirza <asadbmirza97@gmail.com>
2024-10-05 22:58:03 -0400

    Added verification to routes

--- Frontend/src/pages/Details.jsx
+++ Frontend/src/pages/Details.jsx
@@ -3,6 +3,9 @@ import CustomForm from '../components/CustomForm';
 import FormField from '../components/FormField';
 import ListField from '../components/ListField';
 import * as Yup from "yup";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const schema = Yup.object().shape({
   skills: Yup.array()
@@ -17,6 +20,28 @@ const schema = Yup.object().shape({
 
 const Details = () => {
   const [skills, setSkills] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchSkills = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/skills', {
+          withCredentials: true,
+        });
+
+        console.log(response.data);
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          console.log(`You are not authenticated`);
+          navigate('/login'); // Redirect to login page
+        } else {
+          console.error("Error fetching skills:", error);
+        }
+      }
+    };
+
+    fetchSkills();
+  }, [navigate]);
 
   const addSkill = (value, setFieldValue) => {
     if (value && !skills.includes(value)) {
