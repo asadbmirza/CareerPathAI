3d49c1861b5843652577442a6b9a063a37be2219
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-10-05 22:44:22 -0400

    feat: added main page

--- Frontend/src/components/SuggestionCard.jsx
+++ Frontend/src/components/SuggestionCard.jsx
@@ -1,6 +1,13 @@
+import ImageCard from "./ImageCard"
+import { SuggestionCardStyle } from "../styles/mainpage"
+import CardText from "./CardText"
+
 const SuggestionCard = ( { title, description, skills } ) => {
   return (
-    <h1>hallo</h1>
+    <SuggestionCardStyle>
+      <CardText title={title} description={description} skills={skills}></CardText>
+      <ImageCard></ImageCard>
+    </SuggestionCardStyle>
   )
 }
 
--- Frontend/src/pages/Mainpage.jsx
+++ Frontend/src/pages/Mainpage.jsx
@@ -1,11 +1,22 @@
 import { HeaderText } from "../styles/mainpage"
 import SuggestionCard from "../components/SuggestionCard"
+import { RegisterButton } from "../styles/formstyles"
+import { useNavigate } from "react-router-dom"
+
 
 const Mainpage = () => {
+  const navigate = useNavigate();
+
   return (
     <main>
       <HeaderText>Based on your information these are potential steps you can take...</HeaderText>
-      <SuggestionCard></SuggestionCard>
+      <SuggestionCard title="First, check out this course..." description="12323 candidates in a similar position took this step" skills={["why", "do", "re", "me", ]}></SuggestionCard>
+      <SuggestionCard title="Get involved in the community..." description="bye" skills={["why"]}></SuggestionCard>
+      <SuggestionCard title="Connect, contribute, and grow..." description="bye" skills={["why"]}></SuggestionCard>
+      <SuggestionCard title="Kick things off with confidence..." description="bye" skills={["why"]}></SuggestionCard>
+      <SuggestionCard title="Discover what's next for you..." description="bye" skills={["why"]}></SuggestionCard>
+      <RegisterButton>Generate Another Career Path</RegisterButton>
+      <RegisterButton onClick={() => {navigate("/details")}}>Edit Skills and Interests</RegisterButton>
     </main>
   )
 }
